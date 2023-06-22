1da743be87aa8585887c9953e33430684fb7c560
etokiryau <123979827+etokiryau@users.noreply.github.com>
2023-06-23 00:32:39 +0500

    update

--- src/components/ui/progressDates/ProgressDates.tsx
+++ src/components/ui/progressDates/ProgressDates.tsx
@@ -3,6 +3,7 @@ import { FC } from "react";
 import styles from "./progressDates.module.scss";
 
 const ProgressDates: FC = () => {
+    const value: number = 64;
 
     return (
         <div className={styles.dates}>
@@ -25,9 +26,9 @@ const ProgressDates: FC = () => {
                 <div className={styles.dates__content_chart}>
                     <svg id={styles.metric} width="90" height="90" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="50%" cy="50%" r="40" strokeWidth="8" />
-                        <circle cx="50%" cy="50%" r="40" strokeWidth="10" />
+                        <circle cx="50%" cy="50%" r="40" strokeWidth="10" strokeDashoffset={251 - value * 251 / 100} />
                     </svg>
-                    <span>24%</span>
+                    <span>{value}%</span>
                 </div>
 
                 <div className={styles.dates__content_info}>
